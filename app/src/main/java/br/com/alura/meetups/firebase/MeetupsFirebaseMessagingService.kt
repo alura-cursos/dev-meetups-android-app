0aba46dcdfc5cb2a6acd44199666e1e3877c28ee
Alex Felipe <alexfelipevieira@gmail.com>
2020-10-27 11:08:13 -0300

    3.3

--- app/src/main/java/br/com/alura/meetups/firebase/MeetupsFirebaseMessagingService.kt
+++ app/src/main/java/br/com/alura/meetups/firebase/MeetupsFirebaseMessagingService.kt
@@ -1,6 +1,8 @@
 package br.com.alura.meetups.firebase
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import br.com.alura.meetups.R
@@ -12,6 +14,7 @@ import com.google.firebase.messaging.RemoteMessage
 import org.koin.android.ext.android.inject
 
 private const val TAG = "MeetupsFCM"
+private const val IDENTIFICADOR_DO_CANAL = "principal"
 
 class MeetupsFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -30,14 +33,27 @@ class MeetupsFirebaseMessagingService : FirebaseMessagingService() {
         Log.i(TAG, "onMessageReceived: recebeu mensagem de notificacao ${remoteMessage.notification}")
         Log.i(TAG, "onMessageReceived: recebeu mensagem de dados ${remoteMessage.data}")
         val dados = remoteMessage.data
-        val notificacao = NotificationCompat.Builder(this, "principal")
+
+        val gerenciadorDeNotificacoes = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val nome = getString(R.string.channel_name)
+            val descricao = getString(R.string.channel_description)
+            val importancia = NotificationManager.IMPORTANCE_DEFAULT
+            val canal = NotificationChannel(IDENTIFICADOR_DO_CANAL, nome, importancia)
+            canal.description = descricao
+
+            gerenciadorDeNotificacoes.createNotificationChannel(canal)
+        }
+
+        val notificacao = NotificationCompat.Builder(this, IDENTIFICADOR_DO_CANAL)
             .setContentTitle(dados["titulo"])
             .setContentText(dados["descricao"])
             .setSmallIcon(R.mipmap.ic_launcher)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
 
-        val gerenciadorDeNotificacoes = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         gerenciadorDeNotificacoes.notify(1, notificacao)
     }
 
