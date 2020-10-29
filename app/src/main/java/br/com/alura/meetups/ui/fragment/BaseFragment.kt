8f0af7cbb9a0c5403e59d3065d9025dd664d309f
Alex Felipe <alexfelipevieira@gmail.com>
2020-10-29 10:46:17 -0300

    5.3

--- app/src/main/java/br/com/alura/meetups/ui/fragment/BaseFragment.kt
+++ app/src/main/java/br/com/alura/meetups/ui/fragment/BaseFragment.kt
@@ -9,6 +9,8 @@ import androidx.navigation.fragment.findNavController
 import br.com.alura.meetups.NavGraphDirections
 import br.com.alura.meetups.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlin.concurrent.thread
 
 open class BaseFragment(layout: Int) : Fragment(layout) {
 
@@ -37,6 +39,9 @@ open class BaseFragment(layout: Int) : Fragment(layout) {
         when (item.itemId) {
             R.id.lista_eventos_menu_sair -> {
                 FirebaseAuth.getInstance().signOut()
+                thread {
+                    FirebaseInstanceId.getInstance().deleteInstanceId()
+                }
                 vaiParaInicio()
             }
         }
